2240f525ebc96480f621c28d36601100ab38aed6
felipeclarindo <fglpc2@gmail.com>
2025-03-11 22:46:03 -0300

    feat(controller): Adding account controller to manipulate the accounts.

--- src/main/java/com/example/digital_bank_api/controller/AccountController.java
+++ src/main/java/com/example/digital_bank_api/controller/AccountController.java
@@ -4,15 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale.Category;
 
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.digital_bank_api.model.Account;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
-    private List<Category> repository = new ArrayList<>();
+    private List<Account> repository = new ArrayList<>();
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAll() {
+        return ResponseEntity.status(200).body(repository);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        Object response = repository.stream()
+        .filter(a -> a.getId().equals(id))
+        .findFirst();
+        return ResponseEntity.status(200).body(response);
+    }
 }
