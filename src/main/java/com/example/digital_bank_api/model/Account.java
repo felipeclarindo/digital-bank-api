acaf273e3708345dabd7bdd11a42974121b78055
felipeclarindo <fglpc2@gmail.com>
2025-03-11 22:45:21 -0300

    feat(model): Starting validations development.

--- src/main/java/com/example/digital_bank_api/model/Account.java
+++ src/main/java/com/example/digital_bank_api/model/Account.java
@@ -1,38 +1,78 @@
 package com.example.digital_bank_api.model;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-import java.sql.Date;
+import jakarta.annotation.Generated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public class Account {
+
+    private Long id;
+
+    @NotBlank(message = "The field holderName cant is empty.")
     private String holderName;
-    private int holderCpf;
-    private float balance;
+
+    @NotBlank(message = "The field holderCpf cant is empty.")
+    private String holderCpf;
+
+    @NotNull(message = "The started balance is need.")
+    @PositiveOrZero(message = "The started balance needs to be positive.")
+    private BigDecimal startedBalance;
+
+    @Positive(message = "The number needs to be positive.")
     private int number;
-    private int agency;
-    private Date openingDate;
+
+    @NotBlank(message = "The field agency cant is empty.")
+    private String agency;
+
+    @NotNull(message = "The opening date is needed.")
+    @PastOrPresent(message = "The opening date can not be in future.")
+    private LocalDate openingDate;
+
     private AccountState accountState;
     private AccountType accountType;
 
+    public Account(Long id, String holderName, String holderCpf, BigDecimal startedBalance, int number, String agency, Date openingDate, AccountState accountState, AccountType accountType) {
+        this.id = id;
+        this.holderName = holderName;
+        this.holderCpf = holderCpf;
+        this.startedBalance = startedBalance;
+        this.number = number;
+        this.agency = agency;
+        this.openingDate = openingDate;
+        this.accountState = accountState;
+        this.accountType = accountType;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+    
     public String getHolderName() {
         return this.holderName;
     }
 
-    public int getHolderCpf() {
+    public String getHolderCpf() {
         return this.holderCpf;
     }
 
-    public float getBalance() {
-        return this.balance;
+    public BigDecimal getStartedBalance() {
+        return this.startedBalance;
     }
 
     public int getNumber() {
         return this.number;
     }
 
-    public int getAgency() {
+    public String getAgency() {
         return this.agency;
     }
 
-    public Date getOpeningDate() {
+    public LocalDate getOpeningDate() {
         return this.openingDate;
     }
 
