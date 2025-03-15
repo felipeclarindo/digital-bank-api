4133843f95f334fa319af40df4b6ef04637fafed
felipeclarindo <fglpc2@gmail.com>
2025-03-14 22:56:16 -0300

    feat(controller): Adding the route to get api description and the route to create an account.

--- src/main/java/com/example/digital_bank_api/controller/AccountController.java
+++ src/main/java/com/example/digital_bank_api/controller/AccountController.java
@@ -1,37 +1,70 @@
 package com.example.digital_bank_api.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale.Category;
+import java.util.Map;
+import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.digital_bank_api.model.Account;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
     private List<Account> repository = new ArrayList<>();
 
-    @GetMapping("/")
-    public ResponseEntity<Object> getAll() {
+    @GetMapping
+    public ResponseEntity<Object> getAll() {        
+        if (repository.isEmpty()) {
+            return ResponseEntity.status(404).body("No accounts added.");
+        }
         return ResponseEntity.status(200).body(repository);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Object> getById(@PathVariable Long id) {
         Object response = repository.stream()
-        .filter(a -> a.getId().equals(id))
-        .findFirst();
+        .filter(account -> account.getId().equals(id))
+        .findFirst(); 
+
+        if (response.equals(Optional.empty())) {
+            log.info("Error to get the account with id equals to " + id);
+            return ResponseEntity.status(404).body("The account with id " + id + " wasn't found.");
+        }
+        log.info("The account with id " + id + "was found");
         return ResponseEntity.status(200).body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody Account account) {
+        
+        if (account == null) {
+            return ResponseEntity.status(404).body("The RequestBody can not be empty.");
+        }
+
+        log.info("Registering account \n" + account.toString());
+
+        long newId = repository.size() + 1;
+        account.setId(newId); 
+        
+        repository.add(account);
+        return ResponseEntity.status(201).body(account);
     }
 }
--- src/main/java/com/example/digital_bank_api/controller/ApiController.java
+++ src/main/java/com/example/digital_bank_api/controller/ApiController.java
@@ -1,15 +1,32 @@
 package com.example.digital_bank_api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class ApiController {
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<String> index() {
         String response = "Digital Bank Api - "
         + "Integrante: Felipe Gabriel Lopes Pinheiro Clarindo\n";
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<Object> getApiDescription() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Api is running.");
+        response.put("description", "Api for a digital bank developed in java using Spring");
+        response.put("author", "felipeclarindo");
+        response.put("github-repository", "https://github.com/felipeclarindo/digital-bank-api");
+        response.put("github-author", "https://github.com/felipeclarindo");
         return ResponseEntity.status(200).body(response);
     }
 }
