c7a62dfda09711a0a2bad429d2222c1fbe0bd0b9
felipeclarindo <fglpc2@gmail.com>
2025-03-18 20:45:47 -0300

    refactor(controller): Refactor the logical of the controller.

--- src/main/java/com/example/digital_bank_api/controller/ApiController.java
+++ src/main/java/com/example/digital_bank_api/controller/ApiController.java
@@ -3,7 +3,6 @@ package com.example.digital_bank_api.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
